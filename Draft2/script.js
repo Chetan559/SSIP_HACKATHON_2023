cde3ca3eec50a48b728c1f098580ebe172fe7a50
Chetan Sharma <cschetan559@gmail.com>
2023-10-06 21:25:42 +0530

    new draft

---
+++ Draft2/script.js
@@ -0,0 +1,16 @@
+
+document.getElementById('send-button').addEventListener('click', () => {
+    const userInput = document.getElementById('user-input');
+    const chatMessages = document.getElementById('chat-messages');
+
+    if (userInput.value.trim() !== '') {
+        const userMessage = document.createElement('div');
+        userMessage.classList.add('user-message');
+        userMessage.textContent = userInput.value;
+        chatMessages.appendChild(userMessage);
+
+        // Add your GPT-3 API call here to get the response
+
+        userInput.value = '';
+    }
+});
