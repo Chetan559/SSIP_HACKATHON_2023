c4dec97807bd52791218cd8a3aa23692437d1c59
Chetan Sharma <cschetan559@gmail.com>
2023-11-17 12:41:49 +0530

    front end and backEnd  update 2.0

--- Draft2/script.js
+++ Draft2/script.js
@@ -1,9 +1,7 @@
-// Function to append a message to the chat box
 function appendMessage(sender, message) {
     const chatBox = document.getElementById('chat-box');
     const newMessage = document.createElement('div');
-    
-    newMessage.innerHTML = `<span>${sender}:</span><p>${message}</p>`;
+    newMessage.innerHTML = `<p><b>${sender}:</b>  ${message}</p>`;
     
     if (sender === 'Rule_Mitra') {
         newMessage.className = 'chat-message chat-message--Bot';
@@ -14,22 +12,20 @@ function appendMessage(sender, message) {
     chatBox.appendChild(newMessage);
 }
 
-// Function to send a user message to the backend and handle the response
 function sendMessage() {
     const userMessage = document.getElementById('user-input').value;
     appendMessage('You', userMessage);
 
-     // Send the user message to the backend using a POST request at required server 
-    // CHANGE HTTPS://127.0.0.1:5000 WITH YOUR BACKEND'S DEPLOYMENT ADDRESS 
-    fetch('https://127.0.0.1:5000/predict', {
+    // Send the user message to the backend
+    fetch('http://127.0.0.1:5000/predict', {
         method: 'POST',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ message: userMessage })
+        body: JSON.stringify({message: userMessage })
     })
-    .then(r => r.json())  // Parse the response as JSON
+    .then(r => r.json())
     .then(r => {
         appendMessage('Rule_Mitra', r.answer);
     })
@@ -37,22 +33,20 @@ function sendMessage() {
         console.error('Error:', error);
         appendMessage('Rule_Mitra', 'Apologies, something went wrong. Please try again.');
     });
-
-    document.getElementById('user-input').value = ''; // Clear the input field
+    document.getElementById('user-input').value = ''; //clear input
 }
 
-// Function to handle the Enter key press and send a message
 function handleKeyPress(event) {
     if (event.keyCode === 13) {
-        event.preventDefault(); // Prevent the default Enter key behavior (e.g., new line)
+        event.preventDefault(); // Prevents the default Enter key behavior (e.g., new line)
         sendMessage();
     }
 }
 
-// Attach an event listener to the user input field to handle Enter key presses
+// this will send message  to the input field when Enter key is pressed 
 const userInput = document.getElementById('user-input');
 userInput.addEventListener('keypress', handleKeyPress);
 
-// Automatically scroll to the bottom of the chat box
+// scroll to the bottom of the chat box automatically
 const chatBox = document.getElementById('chat-box');
 chatBox.scrollTop = chatBox.scrollHeight;
