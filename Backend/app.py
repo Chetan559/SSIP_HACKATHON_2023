55a0cb50a8ef809e99380b8b0f67f18e56489b8a
Chetan Sharma <cschetan559@gmail.com>
2025-02-09 00:35:50 +0530

    deploying the project

--- Backend/app.py
+++ Backend/app.py
@@ -14,4 +14,4 @@ def predict():
 
 # Run the Flask app if this script is the main entry point in debug mode 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(host='0.0.0.0', port=5000) 
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,50 +1,52 @@
 function appendMessage(sender, message) {
-    const chatBox = document.getElementById('chat-box');
-    const newMessage = document.createElement('div');
-    newMessage.innerHTML = `<p><b>${sender}:</b>  ${message}</p>`;
-    
-    if (sender === 'Rule_Mitra') {
-        newMessage.className = 'chat-message chat-message--Bot';
-    } else {
-        newMessage.className = 'chat-message chat-message--User';
-    }
-    
-    chatBox.appendChild(newMessage);
-    chatBox.scrollTop = chatBox.scrollHeight;
+  const chatBox = document.getElementById("chat-box");
+  const newMessage = document.createElement("div");
+  newMessage.innerHTML = `<p><b>${sender}:</b>  ${message}</p>`;
+
+  if (sender === "Rule_Mitra") {
+    newMessage.className = "chat-message chat-message--Bot";
+  } else {
+    newMessage.className = "chat-message chat-message--User";
+  }
+
+  chatBox.appendChild(newMessage);
+  chatBox.scrollTop = chatBox.scrollHeight;
 }
 
 function sendMessage() {
-    const userMessage = document.getElementById('user-input').value;
-    appendMessage('You', userMessage);
+  const userMessage = document.getElementById("user-input").value;
+  appendMessage("You", userMessage);
 
-    // Send the user message to the backend
-    fetch('http://127.0.0.1:5000/predict', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({message: userMessage })
+  // Send the user message to the backend
+  fetch("ssip_hackathon_2023.railway.internal/predict", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: userMessage }),
+  })
+    .then((r) => r.json())
+    .then((r) => {
+      appendMessage("Rule_Mitra", r.answer);
     })
-    .then(r => r.json())
-    .then(r => {
-        appendMessage('Rule_Mitra', r.answer);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        appendMessage('Rule_Mitra', 'Apologies, something went wrong. Please try again.');
+    .catch((error) => {
+      console.error("Error:", error);
+      appendMessage(
+        "Rule_Mitra",
+        "Apologies, something went wrong. Please try again."
+      );
     });
-    document.getElementById('user-input').value = ''; //clear input
+  document.getElementById("user-input").value = ""; //clear input
 }
 
 function handleKeyPress(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault(); // Prevents the default Enter key behavior (e.g., new line)
-        sendMessage();
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault(); // Prevents the default Enter key behavior (e.g., new line)
+    sendMessage();
+  }
 }
 
-// this will send message  to the input field when Enter key is pressed 
-const userInput = document.getElementById('user-input');
-userInput.addEventListener('keypress', handleKeyPress);
-
+// this will send message  to the input field when Enter key is pressed
+const userInput = document.getElementById("user-input");
+userInput.addEventListener("keypress", handleKeyPress);
